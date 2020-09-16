cbee67d3f4f1d56952df3b3dc8a5f87640d9d9ab
Gupta Shrinath <guptashrinath9@gmail.com>
2020-09-16 17:41:09 +0530

    Remove ABI & address

--- js/config.js
+++ js/config.js
@@ -1,18 +1,2 @@
-var contractABI = [
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "userCount",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	}
-];
-var contractAddress = '0xDd383718a59E0a3C4A195144Be3e57c5554b38fD';
+var contractABI;
+var contractAddress;
