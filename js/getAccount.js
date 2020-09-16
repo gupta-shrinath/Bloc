a9c568b4aa93137f0bc7e482d5d6b43ec606f303
Gupta Shrinath <guptashrinath9@gmail.com>
2020-09-16 17:39:53 +0530

    Add console.log

--- js/getAccount.js
+++ js/getAccount.js
@@ -2,7 +2,7 @@ web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
 async function getAccount() {
     let accounts = await web3.eth.getAccounts();
     web3.eth.defaultAccount = accounts[0];
-    console.log(web3.eth.defaultAccount);
+    console.log(web3.eth.defaultAccount + ' account detected');
     return web3.eth.defaultAccount;
 }
 
